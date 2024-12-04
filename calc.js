59788f20803296af482b014bde48f40a9a822b0a
ANAS EN-NAQADI <anas.ennaqadi0@gmail.com>
2024-12-04 13:59:48 +0100

    new design + fix bugs

--- calc.js
+++ calc.js
@@ -2,30 +2,38 @@ let comp = '';
 
 function appendToDisplay(value) {
     comp += value;
-    document.getElementById("result").textContent = comp;
+    document.getElementById("result1").value = comp;
 }
 
 function clearT() {
    
     comp = "";
     
-    document.getElementById("result").textContent = '';
+    document.getElementById("result1").value = '';
 }
 
 function deleteC() {
+    console.log(comp)
     if (comp.length > 0) {
         comp = comp.slice(0, -1);
-        document.getElementById("result").textContent = comp;
+        console.log("test",comp)
+        document.getElementById("result1").value = comp;
     }
 }
 
 function calculateResult() {
     try {
-        const result = eval(comp);
-        document.getElementById('result').textContent = result;
-        comp = result.toFixed(2).toString();
+        const result1 = eval(comp);
+        if(Number.isInteger(result1))
+            document.getElementById('result1').value  =
+         result1.toString();
+        else
+        document.getElementById('result1').value  = result1.toFixed(2).toString();
+
+        comp = result1.toString();
+
     } catch (error) {
-        document.getElementById('result').textContent = 'Error';
+        document.getElementById('result1').value = 'Error';
     }
 }
 document.getElementById('calculator-form').addEventListener('submit', function (e) {
